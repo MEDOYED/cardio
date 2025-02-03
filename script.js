273b641aa5616df750e780dcb71ab3862847339e
MEDOYED <mmax26072005@gmail.com>
2025-02-03 23:55:29 +0100

    get location by geolocation API

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      // const latitude = position.coords.latitude;
+
+      const { longitude } = position.coords;
+      // const longitude = position.coords.longitude;
+
+      console.log(latitude, longitude);
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},5675m`
+      );
+    },
+    function () {
+      alert('Неможливо отримати ваше місцезнаходження');
+    }
+  );
+}
